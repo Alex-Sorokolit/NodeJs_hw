dc660fe73d7f36d8ded96f6a0a8cc8f429a70e4e
Alex-Sorokolit <104554703+Alex-Sorokolit@users.noreply.github.com>
2023-02-09 16:33:09 +0200

    db

---
+++ contacts.js
@@ -0,0 +1,4 @@
+console.log("contacts file");
+const contacts = ["alex", "gojira"];
+const users = ["Goblin", "Teart"];
+module.exports = { contacts, users };
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("wellcome to node js");
+const { contacts, users } = require("./contacts");
+
+console.log(contacts);
+console.log(users);
